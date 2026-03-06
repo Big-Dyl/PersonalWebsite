6043eb3c5a38a9f1ba8aa136971d3b078a740005
Dylan Liu <liudj5@uci.edu>
2026-03-06 01:41:26 -0800

    minor bug fixes + finished news and music pages

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+function sortPage(){
+    const sortBy = document.getElementsByClassName("sortDropdown")[0].value;
+    const content = document.getElementsByClassName("container-items")[0]
+    const items = Array.from(content.querySelectorAll(".item"));
+    if(sortBy === "name"){
+        items.sort((a, b)=>{
+            return a.querySelector(".headline").innerHTML.localeCompare(b.querySelector(".headline").innerHTML);
+        })
+    } else if(sortBy === "recent"){
+        items.sort((a, b)=>{
+            return new Date(b.querySelector(".date").innerHTML.slice(1, -1)) - new Date(a.querySelector(".date").innerHTML.slice(1, -1));
+        })
+    }  else if(sortBy === "best"){
+        items.sort((a, b)=>{
+            return parseInt(a.dataset.dylan_ranking) - parseInt(b.dataset.dylan_ranking);
+        })
+    }
+    content.innerHTML = "";
+    for(const i of items){
+        content.appendChild(i);
+    }
+}
